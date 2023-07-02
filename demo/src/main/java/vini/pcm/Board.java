1a36b32042de1477aa9f57eaa4e950db65ecf810
Vinicius Medeiros <111023519+ViniciusPCMedeiros@users.noreply.github.com>
2023-07-01 22:44:40 -0300

    a

---
+++ demo/src/main/java/vini/pcm/Board.java
@@ -0,0 +1,16 @@
+package vini.pcm;
+
+import javafx.scene.layout.Pane;
+
+public class Board extends Pane {
+
+    public Board() {
+        // Configuração do painel do tabuleiro
+        setPrefSize(800, 600);
+        // Adicione aqui os elementos gráficos e lógica do tabuleiro
+        // Exemplo: cartas, zonas de monstros, zonas de magias, etc.
+    }
+
+    // Métodos e lógica adicionais do tabuleiro
+
+}

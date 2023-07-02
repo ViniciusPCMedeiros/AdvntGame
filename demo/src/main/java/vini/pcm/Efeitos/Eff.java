102021e3a31d67333e766f1df5d5e5bd61c6468a
Vinicius Medeiros <111023519+ViniciusPCMedeiros@users.noreply.github.com>
2023-07-01 22:43:46 -0300

    tt

--- demo/src/main/java/vini/pcm/Efeitos/Eff.java
+++ demo/src/main/java/vini/pcm/Efeitos/Eff.java
@@ -1,5 +1,45 @@
 package vini.pcm.Efeitos;
 
-public class Eff {
+public class Eff implements Efct {
+    private Target target ;
+
+    public Eff(Target target){
+        this.target = target;
+    }
+    
+
+    @Override
+    public void ativarEfeito() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'ativarEfeito'");
+    }
+
+    @Override
+    public void aplicarEfeito() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'aplicarEfeito'");
+    }
+
+    @Override
+    public void desfazerEfeito() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'desfazerEfeito'");
+    }
+
+    @Override
+    public boolean podeAtivar() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'podeAtivar'");
+    }
+
+
+    public Target getTarget() {
+        return target;
+    }
+
+
+    public void setTarget(Target target) {
+        this.target = target;
+    }
 
 }
